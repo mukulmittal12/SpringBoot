c0e4b698b9e545501e2329d20d3243db60f0851c
OYO <oyo@administrators-MacBook-Pro-7.local>
2018-12-27 13:27:39 +0530

    saved

---
+++ course-api-data/src/main/java/io/javabrains/springbootstarter/course/CourseService.java
@@ -0,0 +1,50 @@
+package io.javabrains.springbootstarter.course;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CourseService {
+		
+	@Autowired
+	private CourseRepository courseRepository;
+	
+	
+	public List<Course> getAllCourses(String topicId){
+		//return topics;
+		List<Course> courses = new ArrayList<>();
+		courseRepository.findByTopicId(topicId)
+		.forEach(courses::add);
+		return courses;
+	}
+	
+	public Course getCourse(String id) {
+		//return topics.stream().filter(t -> t.getId().equals(id)).findFirst().get();
+		return courseRepository.findById(id).orElse(null);
+	}
+
+	public void addCourse(Course course) {
+		//topics.add(topic);
+		courseRepository.save(course);
+	}
+
+	public void updateCourse(Course course) {
+		/*for(int i=0; i<topics.size(); i++) {
+			Topic t = topics.get(i);
+			if(t.getId().equals(id)) {
+				topics.set(i, topic);
+				return;
+			}
+		}
+		*/
+		courseRepository.save(course);
+	}
+
+	public void deleteCourse(String id) {
+		//topics.removeIf(t -> t.getId().equals(id));
+		courseRepository.deleteById(id);
+	}
+}
